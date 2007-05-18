bad581790e3c1b63264626d15a692b6ef81b9f9d
jacob.brunson <jacob.brunson@011ced47-bf30-0410-9364-95c1d857d74a>
2007-05-18 14:26:11 +0000

    initial import

---
+++ trunk/index.php
@@ -0,0 +1,7 @@
+<form method="post" action="result.php">
+Title: <input type="text" name="title"/><br/>
+Enter Octave Code:<br/>
+<textarea name="code" cols="100" rows="20"></textarea>
+<br/>Graph Type:<select name="image_type"><option value="svg" SELECTED>Inline SVG</option><option value="png">PNG (may print better)</option></select>
+<br/><input type="submit">
+</form>
